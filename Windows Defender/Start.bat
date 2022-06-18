c7b7e0da88fc0eff653dd8e52a90e4b8511b4d9a
Smolich404 <86857963+Smolich404@users.noreply.github.com>
2022-06-18 16:42:28 +0200

    Add files via upload

--- Windows Defender/Start.bat
+++ Windows Defender/Start.bat
@@ -1,29 +1,32 @@
+MODE 90,10
 @echo off
-set "version=0.1"
-title Windows Defender
-goto :Start
+set "version=0.2"
+title Opline - Defender
+goto :WU
 
-:Start
+:WU
 cls
 echo.========================================================================================
 echo.	
-echo.	Press [1] to enable Windows Defender
-echo.	Press [2] to disable Windows Defender
-echo.
-echo.	Press [X] to quit
+echo.	Press [1] to Enable Windows Defender
+echo.	Press [2] to Disable Windows Defender
+echo.	Press [X] to Quit
 echo.
 echo.========================================================================================
 choice /C:12X /N /M "Enter Your Choice: "
 if errorlevel == 3 goto :Quit
-if errorlevel == 2 goto :Disable
-if errorlevel == 1 goto :Enable
+if errorlevel == 2 goto :DisableWU
+if errorlevel == 1 goto :EnableWU
 
-:Enable
-@start /b "Opline - TrustedInstaller" "%~dp0bin\NSudoLG.exe" -U:T -P:E "%~dp0bin\Enable_Windows_Defender.bat"
+:Quit
+exit
+
+:EnableWU
+@start /b "Opline - Enable WinDefend - TrustedInstaller" "%~dp0bin\NSudoLG.exe" -U:T -P:E "%~dp0bin\Enable_Windows_Defender.bat"
 goto :EOF
 
-:Disable
-@start /b "Opline - TrustedInstaller" "%~dp0bin\NSudoLG.exe" -U:T -P:E "%~dp0bin\Disable_Windows_Defender.bat"
+:DisableWU
+@start /b "Opline - Disable WinDefend - TrustedInstaller" "%~dp0bin\NSudoLG.exe" -U:T -P:E "%~dp0bin\Disable_Windows_Defender.bat"
 goto :EOF
 
 :EOF
